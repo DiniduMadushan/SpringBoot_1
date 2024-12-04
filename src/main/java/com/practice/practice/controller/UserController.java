28843a934caa243aca1b3ce205dd045981dcaf67
DiniduMadushan <dinidumadushan15576@gmail.com>
2024-12-04 06:34:48 +0530

    add GET and POST mappings

--- src/main/java/com/practice/practice/controller/UserController.java
+++ src/main/java/com/practice/practice/controller/UserController.java
@@ -1,14 +1,26 @@
 package com.practice.practice.controller;
 
+import com.practice.practice.dto.UserDTO;
+import com.practice.practice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping(value = "api/v1/")
 public class UserController {
-    @GetMapping("/getUser")
-    public String getUser(){
-        return "one User";
+    @Autowired
+    private UserService userService;
+    @GetMapping("/getusers")
+    public List<UserDTO> getUsers(){
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/adduser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
     }
 
 }
--- src/main/java/com/practice/practice/repo/UserRepo.java
+++ src/main/java/com/practice/practice/repo/UserRepo.java
@@ -1,4 +1,9 @@
 package com.practice.practice.repo;
 
-public interface UserRepo {
+import com.practice.practice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
 }
